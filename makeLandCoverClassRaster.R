eef1820903ff08741069b0269db433cd684ce196
IsoldeLS <r.i.lane.shaw@gmail.com>
2026-03-09 15:29:59 -0400

    updates to paths

--- makeLandCoverClassRaster.R
+++ makeLandCoverClassRaster.R
@@ -1,63 +1,9 @@
 
 
-library("conflicted")
+#library("conflicted")
 library("SpaDES")
-library("googledrive")
 library("terra")
 library("LandR")
-library("sf")
-
-
-# #get rasterToMatch
-# print("get rasterTomatch from local drive")
-# rasterToMatch <- terra::rast(file.path("C:/Users/RALAS6/Documents/Repositories/Data/LCCProducts/LCC05/LCC2005_V1_4a.tif"))
-# 
-# 
-# #get StudyArea shapefile
-# print("get studyArea shapefile from local drive")
-# studyArea <- terra::vect(file.path("C:/Users/RALAS6/Documents/Repositories/Data/studyAreaAB/studyAreaAB.shp"))
-# 
-# #postProcess studyArea
-# studyArea <- reproducible::postProcess(studyArea,
-#                                        destinationPath = getwd(),
-#                                        filename2 = "studyArea", 
-#                                        useTerra = TRUE,
-#                                        fun = "terra::vect", #use the function vect
-#                                        targetCRS = crs(rasterToMatch), #make crs same as rasterToMatch
-#                                        overwrite = TRUE,
-#                                        verbose = TRUE)
-# 
-# rasterToMatch
-# 
-# #crop and mask rasterToMatch
-# rasterToMatch <- terra::mask(terra::crop(rasterToMatch, studyArea), studyArea) 
-# names(rasterToMatch) <- "rasterToMatch"
-# # clearPlot()
-# # Plot(sim$rasterToMatch)
-# plot(rasterToMatch, colNA = "grey")
-# 
-# LCC20 <- terra::rast(file.path("C:/Users/RALAS6/Documents/Repositories/Data/LCCProducts/LCC20/landcover-2020-classification.tif"))
-# LCC20
-# #LCC20 <- as.factor(LCC20)
-# #terra::unique(LCC20)
-# plot(LCC20)
-# 
-# reclassTab <- read.csv("C:/Users/RALAS6/Documents/Repositories/SpaDES/inputs/LCC20_reclass.csv", sep = ";", header = TRUE)
-# 
-# landCoverClassRaster <- terra::classify(LCC20, reclassTab)
-# landCoverClassRaster
-# is.factor(landCoverClassRaster)
-# 
-# LCCRas <- postProcessTerra(landCoverClassRaster, studyArea = studyArea)
-# LCCRas
-# plot(LCCRas)
-# as.factor(LCCRas)
-# LCCRas <- postProcessTerra(LCCRas, rasterToMatch = rasterToMatch)
-# LCCRas
-# is.factor(LCCRas)
-# terra::unique(LCCRas)
-
-
 
 #############################################################################################
 outputsDir <- checkPath("../../outputs", create = TRUE)
@@ -69,23 +15,26 @@ setPaths(modulePath = file.path("../../modules"),
          inputPath = inputsDir,
          outputPath = outputsDir)
 
-rasterToMatchLocation <- file.path(inputsDir, "LCC2005_V1_4a.tif")
+rasterToMatchLocation <- file.path(inputsDir, "ALFL-meanBoot_BCR-60_studyArea_AB_BCR6")
 rasterToMatch <- terra::rast(rasterToMatchLocation)
 #rasterToMatch <- prepInputsLCC(year = 2005)
 
 #get StudyArea shapefile
 print("get studyArea shapefile from local drive")
 
-studyArea <- terra::vect("../../inputs/studyArea/studyArea_AB_BCR6/studyArea_AB_BCR6.shp")
+# specify studyArea
+locationStudyArea <- checkPath(file.path(Paths$inputPath, "studyArea/studyArea_AB_BCR6"), create = TRUE)
+.studyAreaName <- "studyArea_AB_BCR6.shp"
+studyArea <- terra::vect(file.path(locationStudyArea,.studyAreaName))
 
 #postProcess studyArea
 studyArea <- reproducible::postProcess(studyArea,
                                        destinationPath = getwd(),
-                                       filename2 = "studyArea", 
+                                       #filename2 = "studyArea", 
                                        #useTerra = FALSE,
                                        #fun = "sf", #use the function vect
                                        targetCRS = crs(rasterToMatch), #make crs same as rasterToMatch
-                                       overwrite = TRUE,
+                                       overwrite = FALSE,
                                        verbose = TRUE)
 
 rasterToMatch <- crop(rasterToMatch, studyArea)
@@ -107,7 +56,7 @@ valsLCC10 <- values(LCC10)
 uniqueValsLCC10 <- sort(unique(valsLCC10))
 
 
-reclassTab <- read.csv("C:/Users/RALAS6/Documents/Repositories/SpaDES/inputs/LCC10_reclass.csv", sep = ";", header = TRUE)
+reclassTab <- read.csv("../../inputs/LCC10_reclass.csv", sep = ";", header = TRUE)
 reclassTab$landCoverClass <- as.factor(reclassTab$landCoverClass)
 landCoverClassRaster <- terra::classify(LCC10, reclassTab)
 landCoverClassRaster
@@ -120,3 +69,4 @@ terra::writeRaster(x = landCoverClassRaster,
                    filetype= "GTiff",
                    gdal="COMPRESS=NONE",
                    overwrite = TRUE)
+
